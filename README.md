5fb0b9a41c6350f4d12568a388a6a1a7f6b13503
guillermotoppph <guillermotoppph@gmail.com>
2026-03-30 16:36:59 -0300

    Agregado CSS

--- README.md
+++ README.md
@@ -5,8 +5,4 @@ Práctico del Curso de React Básico de LinkedIn
 
 ## React Compiler
 
-The React Compiler is not enabled on this template because of its impact on dev & build performances. To add it, see [this documentation](https://react.dev/learn/react-compiler/installation).
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+El uso de Index para listas que no se van a modificar
--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -1,38 +1,30 @@
 import React from "react";
+import "./Catalogo.css"; 
+
 const Catalogo = () => {
     const productos = [
-        {
-            id: 1,
-            nombre: 'Camiseta',
-            precio: 20.99
-        },
-        {   id: 2,
-            nombre: 'Pantalon',
-            precio: 20.22
-        },
-        {   id: 3,
-            nombre: 'zapatos',
-            precio: 21.22
-        },
-        {   id: 4,
-            nombre: 'corbata',
-            precio: 23.22
-        }
-        
+        { id: 1, nombre: 'Camiseta', precio: 20.99 },
+        { id: 2, nombre: 'Pantalon', precio: 20.22 },
+        { id: 3, nombre: 'Zapatos', precio: 21.22 },
+        { id: 4, nombre: 'Corbata', precio: 23.22 }
     ];
 
     return (
-        <>
-            <h2> Catálogo de Productos </h2>
-            <ul>
-                {productos.map(
-                    (producto) => (
-                        <li> {producto.id}: {producto.nombre} - $ {producto.precio} </li>
-                    )
-                )}
-                
+        <div className="catalogo-container">
+            <h2 className="catalogo-title">Catálogo de Productos</h2>
+            <ul className="md3-list">
+                {productos.map((producto, index) => (
+                    /* Mantenemos la key={index} para tu clase de mañana */
+                    <li key={index} className="md3-list-item">
+                        <div className="md3-list-content">
+                            <span className="md3-item-name">{producto.nombre}</span>
+                        </div>
+                        <span className="md3-item-price">${producto.precio}</span>
+                    </li>
+                ))}
             </ul>
-        </>
+        </div>
     );
 }
+
 export default Catalogo;

dfa76be3c872e355c68ffca479c2d9f4ad518de0
guillermotoppph <guillermotoppph@gmail.com>
2026-03-31 21:14:37 -0300

    Fragment component para mejorar la utilización del nodo principal de un componente

--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./Catalogo.css"; 
 
 const Catalogo = () => {
@@ -10,7 +10,7 @@ const Catalogo = () => {
     ];
 
     return (
-        <div className="catalogo-container">
+        <Fragment>
             <h2 className="catalogo-title">Catálogo de Productos</h2>
             {productos.length === 0 ?
             (<p> No hay productos disponibles en el catálogo</p>
@@ -29,7 +29,7 @@ const Catalogo = () => {
                     ))}
                 </ul>
             )}
-        </div>
+        </Fragment>
     );
 }
 
